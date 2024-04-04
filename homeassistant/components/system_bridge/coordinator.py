5cb45d637d67f006af4f76c3e5d6b47a1a55bfc2
Aidan Timson <aidan@timmo.dev>
2024-04-04 23:34:05 +0000

    Add missing check

--- homeassistant/components/system_bridge/coordinator.py
+++ homeassistant/components/system_bridge/coordinator.py
@@ -147,6 +147,8 @@ class SystemBridgeDataUpdateCoordinator(DataUpdateCoordinator[SystemBridgeData])
             self.websocket_client.connected,
         )
 
+        await self.check_websocket_connected()
+
         if not self.registered:
             try:
                 self.hass.async_create_background_task(
